2f4910ed1eb5df3c73c172b5af6c0c90e7fab049
fbian <al221911909@gmail.com>
2023-08-30 15:51:16 -0600

    task controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class TaskController extends Controller
+{
+    
+    public function index()
+    {
+        $completedTasks = Task::where('completed', 1)->get();
+        $pendingTasks = Task::where('completed', 0)->get();
+
+        return view('tasks.index', compact('completedTasks', 'pendingTasks'));
+    }
+    public function getTasks()
+    {
+        $completedTasks = Task::where('completed', 1)->get();
+        $pendingTasks = Task::where('completed', 0)->get();
+
+        return response()->json([
+            'completedTasks' => $completedTasks,
+            'pendingTasks' => $pendingTasks,
+        ]);
+    }
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json(['task' => $task]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        $user = auth()->user();
+
+        $user->tasks()->create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'completed' => 0
+        ]);
+
+        return response()->json(['message' => 'Tarea creada exitosamente']);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+
+        $task->update([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+
+        return response()->json(['message' => 'Tarea actualizada exitosamente']);
+    }
+
+    public function complete($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update(['completed' => 1]);
+
+        return response()->json(['message' => 'Tarea completada exitosamente']);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return response()->json(['message' => 'Tarea eliminada exitosamente']);
+    }
+}
