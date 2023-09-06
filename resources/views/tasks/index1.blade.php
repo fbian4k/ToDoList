10f0c2e3bdb4730908d0f536f7f99243058d7c11
fbian <al221911909@gmail.com>
2023-09-05 20:58:48 -0600

    toastr.options once

--- resources/views/tasks/index1.blade.php
+++ resources/views/tasks/index1.blade.php
@@ -126,13 +126,21 @@
 
 <script>
     toastr.options = {
-    "positionClass": "toast-top-right", 
-    "preventDuplicates": true, 
-    "showDuration": 300, 
-    "hideDuration": 1000, 
-    "timeOut": 5000, 
-    "extendedTimeOut": 1000, 
-    "toastClass": "toast", 
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
     };
 
     $(document).ready(function() {
@@ -323,44 +331,10 @@
     }
 
     function showSuccessMessage(message) {
-        toastr.options = {
-        "closeButton": false,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": false,
-        "positionClass": "toast-top-right",
-        "preventDuplicates": false,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-        }
         toastr.success(message);
     }
 
     function showErrorMessage(message) {
-        toastr.options = {
-        "closeButton": false,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": false,
-        "positionClass": "toast-top-right",
-        "preventDuplicates": false,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-        }
         toastr.error(message);
     }
 
